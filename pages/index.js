cee3a46b479ba1c66ee1685a4d788f7ae41cc668
Lincoln Berto <lincoln.santospro@gmail.com>
2025-01-20 04:29:24 +0000

    modifica a frase

--- pages/index.js
+++ pages/index.js
@@ -1,5 +1,5 @@
 function Home(){
-    return <h1>Kamilly, eu te amo. E se você também me ama, da uma risadinha!</h1>
+    return <h1>Kamilly, eu te amo. E se você também me ama, da uma risadinha! ❤️</h1>
 }
 
 export default Home;
