19dadbe84d3d4197221568bb50b00df080936e32
eilincoln <lincoln.santospro@gmail.com>
2025-01-29 22:11:19 -0300

    update `index.js` - add mensagem para marcos

--- pages/index.js
+++ pages/index.js
@@ -1,7 +1,6 @@
 function Home() {
   return (
-    <h1>Oi mãeeee! Eu te amoo!! ❤️</h1>
-    <h1>Marcoooooos, obrigado por se preocupar comigo e obrigado por tentar me ajudar!! ❤️</h1>
+    <h1>Oi mãeeee! Eu te amoo!! ❤️ Marcoooooos, obrigado por se preocupar comigo e obrigado por tentar me ajudar!! ❤️</h1>
   );
 }
 
