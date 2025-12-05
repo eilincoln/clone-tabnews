8cb19310f62056fffcfed418f47b64220ad07d3c
Lincoln Berto <lincoln.santospro@gmail.com>
2025-12-05 01:09:30 +0000

    adiciona scripts `lint:check` e `lint:fix`

--- pages/index.js
+++ pages/index.js
@@ -1,4 +1,8 @@
 function Home() {
-    return <h1>Jubs! Eu te amo ❤️👨🏻‍💻 - Você é a melhor irmã do mundo.</h1>;
+  return <h1>Jubs! Eu te amo ❤️👨🏻‍💻 - Você é a melhor irmã do mundo.</h1>;
 }
 export default Home;
+
+function teste() {
+  console.log("teste");
+}
